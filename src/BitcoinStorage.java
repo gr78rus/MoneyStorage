f3e60631e6a70e6aef0dcf23e4c6144d1b833331
Roman Grigorev <gr78rus@gmail.com>
2023-01-05 22:25:02 +0500

    ver 1.0

---
+++ src/BitcoinStorage.java
@@ -0,0 +1,16 @@
+public class BitcoinStorage<T extends Number> implements Storage {
+    T t1;
+    T t2;
+    T t3;
+
+    public BitcoinStorage(T t1, T t2, T t3) {
+        this.t1 = t1;
+        this.t2 = t2;
+        this.t3 = t3;
+    }
+
+    @Override
+    public void umnozhitNaDva() {
+
+    }
+}
---
+++ src/Uslovie.java
@@ -0,0 +1,4 @@
+@FunctionalInterface
+public interface Uslovie<T> {
+    boolean proverka(T t1, T t2, T t3);
+}
